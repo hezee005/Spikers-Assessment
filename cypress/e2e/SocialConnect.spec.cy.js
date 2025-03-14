9219a031383cda8c62b2a7da0f186371834fc8df
hezee005 <garubaibrahim005@gmail.com>
2025-03-14 13:42:14 +0100

    Some refactoring

--- cypress/e2e/SocialConnect.spec.cy.js
+++ cypress/e2e/SocialConnect.spec.cy.js
@@ -1,25 +1,29 @@
 import LoginPage from '../support/pages/loginPage';
 import SocialConnectPage from '../support/pages/socialConnectPage';
 
+const loginPage = new LoginPage();
+const socialConnectPage = new SocialConnectPage()
+
 describe('Social Connect Tests', () => {
+  
   before(() => {
-    LoginPage.loginWithSession();
+    loginPage.loginWithSession();
   });
 
   describe('Navigate to Social Connect', () => {
     it('Visits Social Connect page without 401', () => {
-      SocialConnectPage.visit();
-      SocialConnectPage.clickYouTubeIcon();
-      SocialConnectPage.clickYouTubeBranding();
-      SocialConnectPage.clickUseAnotherAccount();
+      socialConnectPage.visit();
+      socialConnectPage.clickYouTubeIcon();
+      socialConnectPage.clickYouTubeBranding();
+      socialConnectPage.clickUseAnotherAccount();
     });
 
     it('Handles Gmail authentication (Manual Step)', () => {
-      SocialConnectPage.handleManualLogin();
+      socialConnectPage.handleManualLogin();
     });
 
     it('Confirms successful YouTube connection', () => {
-      SocialConnectPage.confirmSuccessfulConnection();
+      socialConnectPage.confirmSuccessfulConnection();
     });
   });
 });
--- cypress/support/pages/loginPage.js
+++ cypress/support/pages/loginPage.js
@@ -1,17 +1,15 @@
-export default new LoginPage();
-
 class LoginPage {
-    visit() {
-      cy.visit('https://demo.spikerz.com/', {
-        auth: { username: 'me', password: 'SmipMe123456' }
-      });
-    }
-  
-    loginWithSession() {
-      cy.session('loginSession', () => {
-        this.visit();
-      });
-    }
+  visit() {
+    cy.visit('https://demo.spikerz.com/', {
+      auth: { username: 'me', password: 'SmipMe123456' }
+    });
   }
-  
-  
+
+  loginWithSession() {
+    cy.session('loginSession', () => {
+      this.visit();
+    });
+  }
+}
+
+export default LoginPage; 
